e809385506e077313267d4ce37f08a112031fd9c
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2023-03-18 21:29:31 -0500

    mini rectangulo rota y cono -2

--- src/Experience.js
+++ src/Experience.js
@@ -1,9 +1,25 @@
+//import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
 export function Experience(){
+
+    const boxRef = useRef();
+
+    useFrame((state, delta) => {
+        boxRef.current.rotation.x += 1 * delta;
+    })
+
     return<>
-        <mesh>
+        <mesh ref={boxRef} position={[0, -3, 0]}>
             <boxGeometry></boxGeometry>
             <meshBasicMaterial color={"mediumpurple"}></meshBasicMaterial>
         </mesh>
+        <mesh> position={[1, 5, 5]}
+            <coneGeometry></coneGeometry>
+            <meshBasicMaterial color={"yellow"}></meshBasicMaterial>
+        </mesh>
     </>
+    
 }
     
