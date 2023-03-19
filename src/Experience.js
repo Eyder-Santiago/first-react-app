ce6c1e87e01bd41b5b3d971adeb32ff7cbf19a07
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2023-03-18 20:24:37 -0500

    primer cajita en three.js

---
+++ src/Experience.js
@@ -0,0 +1,9 @@
+export function Experience(){
+    return<>
+        <mesh>
+            <boxGeometry></boxGeometry>
+            <meshBasicMaterial color={"mediumpurple"}></meshBasicMaterial>
+        </mesh>
+    </>
+}
+    
