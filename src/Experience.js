f7fdbe5bfea495295d13ab5738f84ea3cb579540
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2023-03-24 20:45:31 -0500

    Animación Esfera

--- src/Experience.js
+++ src/Experience.js
@@ -15,8 +15,22 @@ export function Experience(){
     const boxRef = useRef();
     const conoRef = useRef();
     const colorTexture = useTexture('./esferaWood.jpg');
+    const sphereRef = useRef();
     // const sphereRef = useRef()
     // const groupRef = useRef(new Group())
+
+    //Para hacer la animación de la esfera
+    const amplitude = 2;
+    const frequency = 2;
+    let   time = 0;
+    useFrame(() => {
+        // Actualizar la posición vertical de la esfera
+        const x = 0 + Math.sin(time * frequency) * amplitude;
+        sphereRef.current.position.setX(x);
+    
+        // Aumentar el tiempo de animación
+        time += 0.02;
+      });
     
     useFrame((state, delta)=>{
         boxRef.current.rotation.x += 1 * delta  ;
@@ -46,13 +60,13 @@ export function Experience(){
                 <coneGeometry></coneGeometry>
                 <meshStandardMaterial color={"yellow"}></meshStandardMaterial>
             </mesh>
-            <mesh position-z = {3} >
+            <mesh ref={sphereRef} position-z = {3} >
                 <sphereGeometry args={[1, 32, 64]}/>
                 <meshStandardMaterial color={"mediumpurple"} map={colorTexture}/>
             </mesh>
-            <mesh rotation-x={-Math.PI / 2} position-y={-1}>
+            <mesh rotation-x={-Math.PI / 2} position-y={-1.5}>
                 <planeGeometry args={[100, 100]} />
-                <meshStandardMaterial color={"green"}/>
+                <meshStandardMaterial color={"blue"}/>
             </mesh>
         </Center>
     </>
